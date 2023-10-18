b4c771681ab3fc536b08db7d89af2abb8493e0cc
Vahn Gomes <me@vahngomes.dev>
2023-10-18 18:52:45 -0400

    Fixed branch name

--- setup.sh
+++ setup.sh
@@ -9,13 +9,14 @@ REPO_ZIP_URL="https://github.com/Codycody31/Prevent-OCI-Deletion-for-being-idle/
 # Fetch and unzip the repo
 wget $REPO_ZIP_URL -O repo.zip
 unzip repo.zip -d /home/ubuntu/
-mv /home/ubuntu/Prevent-OCI-Deletion-for-being-idle-main $TARGET_DIR
+mv /home/ubuntu/Prevent-OCI-Deletion-for-being-idle-master $TARGET_DIR
 rm repo.zip
 
 # Set up cron
 # Note: This will override any previous cron tasks set up for the script
 # Backup the crontab first
 crontab -l > cron_backup.txt
-echo "* * * * * $TARGET_DIR/startPointlessProcesses.sh >> $TARGET_DIR/log/trackPointlessWork.log 2>&1" | crontab -
+echo "Configuring cron..."
+echo "* * * * * $TARGET_DIR/startPointlessProcesses.sh" | crontab -
 
 echo "Setup complete!"
