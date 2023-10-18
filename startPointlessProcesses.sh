689d3da6c8d59ab82dc6da1a1229e894d8921f66
Vahn Gomes <me@vahngomes.dev>
2023-10-18 18:14:23 -0400

    Adjust CPU Load threshold to 20% and update log messages

--- startPointlessProcesses.sh
+++ startPointlessProcesses.sh
@@ -2,8 +2,25 @@
 
 # Run this script using a "screen" (# apt install screen) ($ man screen)
 
+# Define the lockfile path
+LOCKFILE="/tmp/startPointlessProcesses.lock"
+
+# Check if lockfile exists
+if [ -e "$LOCKFILE" ]; then
+    echo "Another instance of the script is already running. Exiting." >> /home/ubuntu/Prevent-OCI-Deletion-for-being-idle/log/trackPointlessWork.log
+    exit 0
+fi
+
+# Create lockfile
+touch "$LOCKFILE"
+
+# Ensure that lockfile is removed when script exits
+trap "rm -f $LOCKFILE" EXIT
+
+# Change directory to the script's directory
 cd "$(dirname "$0")" || exit
 
+# Define the log file path
 log_file="/home/ubuntu/Prevent-OCI-Deletion-for-being-idle/log/trackPointlessWork.log"
 
 # Function to calculate the current CPU load
@@ -18,7 +35,7 @@ while true; do
     currentCpuLoad=$(get_cpu_load)
     echo "Current CPU Load at $(date): $currentCpuLoad%" >> "$log_file"
 
-    if [ $currentCpuLoad -le 20 ]; then  # Adjusted the threshold to 30% for some buffer
+    if [ $currentCpuLoad -le 20 ]; then  # Adjusted the threshold to 20% for some buffer
         echo "CPU Load below threshold at $(date). Spawning 5 instances of cpuUser.sh." >> "$log_file"
         
         # Spawn 5 instances of cpuUser.sh concurrently
