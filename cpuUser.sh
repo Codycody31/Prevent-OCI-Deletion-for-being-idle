e87f51ed60692770f81ac1ca4f8769d8e7751b3b
Vahn Gomes <me@vahngomes.dev>
2023-10-22 22:44:19 -0400

    Reorganize bash scripts for CPU usage

--- cpuUser.sh
+++ cpuUser.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-now=$(date +%s)sec
-
 x=0
 
 while [ $x -le 50000 ]
 do
-  echo $(( $RANDOM % 5000000000000 + 100000000000000000000 )) > /dev/null
-  x=$(( $x + 1 ))
+  echo $(( RANDOM % 5000000000000 + 100000000000000000000 )) > /dev/null
+  x=$(( x + 1 ))
 done
--- cpuUserArray.sh
+++ cpuUserArray.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-now=$(date +%s)sec
 x=0
 declare -A pointlessNumbersArray
 
@@ -7,6 +6,6 @@ while [ $x -le 100000 ]
 do
   currentArrayLength=${#pointlessNumbersArray[@]}
   newArrayLength=$(( currentArrayLength + 1 ))
-  pointlessNumbersArray[$newArrayLength]+=$(( $RANDOM % 5000000000000 + 100000000000000000000 ))
-  x=$(( $x + 1 ))
+  pointlessNumbersArray[$newArrayLength]+=$(( RANDOM % 5000000000000 + 100000000000000000000 ))
+  x=$(( x + 1 ))
 done
