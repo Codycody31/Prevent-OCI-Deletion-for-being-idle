d4efcf219d91f5942d45fe3d509059235a50dba5
Vahn Gomes <me@vahngomes.dev>
2023-10-22 22:39:50 -0400

    feat: Add log directory creation in startPointlessProcesses.sh script

--- startPointlessProcesses.sh
+++ startPointlessProcesses.sh
@@ -11,6 +11,9 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 # Define the log file path
 log_file="$SCRIPT_DIR/log/trackPointlessWork.log"
 
+# Ensure that the log directory exists
+mkdir -p "$SCRIPT_DIR/log"
+
 # Function to calculate the current CPU load
 get_cpu_load() {
     echo $((100 - $(vmstat 1 2 | tail -1 | awk '{print $15}')))
@@ -32,7 +35,7 @@ fi
 touch "$LOCKFILE"
 
 # Ensure that lockfile is removed when script exits
-trap "rm -f $LOCKFILE" EXIT
+trap 'rm -f $LOCKFILE' EXIT
 
 # Change directory to the script's directory
 cd "$SCRIPT_DIR" || exit
@@ -41,7 +44,7 @@ cd "$SCRIPT_DIR" || exit
 log "Starting startPointlessProcesses.sh at $(date). Monitoring CPU Load..."
 
 # If log file is too big, truncate it
-if [ $(wc -c <"$log_file") -gt 1000000 ]; then
+if [ "$(wc -c <"$log_file")" -gt 1000000 ]; then
     # Make var that is the filename with the date appended
     OLDLOGFILE="trackPointlessWork-$(date +"%Y-%m-%d-%H-%M-%S").log"
 
@@ -59,11 +62,11 @@ while true; do
     log "Current CPU Load at $(date): $currentCpuLoad%"
 
     # if CPU load is below 20%, spawn 5 instances of cpuUser.sh
-    if [ $currentCpuLoad -le 20 ]; then  # Adjusted the threshold to 20% for some buffer
+    if [ "$currentCpuLoad" -le 20 ]; then  # Adjusted the threshold to 20% for some buffer
         log "CPU Load below threshold at $(date). Spawning 5 instances of cpuUser.sh."
         
         # Spawn 5 instances of cpuUser.sh concurrently
-        for i in {1..5}; do
+        for _ in {1..5}; do
             /bin/bash "$SCRIPT_DIR/cpuUser.sh" &
         done
         
